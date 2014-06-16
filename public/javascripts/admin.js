2ef7d5b7721ca5f673fbafd9e852f71ed4b79ad1
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-06-16 15:23:48 -0700

    replacing with input field

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
 
-  var live;
+	$('#edit').on('click', function() {
+		//console.log('shit is popping!');
+		$('.editable').toggleClass('live');
 
-  $('#edit').on('click', function() {
-  	//console.log('shit is popping!');
-  	$('.editable').toggleClass('live');
-  	live = $('.live');
+	});
 
-  });
+/*
+	$('td').on('hover', function() {
+		if ($(this).hasClass('live')) {
+			$(this).css('background-color', 'yellow');
+		};
+	});
+*/
 
-
-  $('td').on('click', function() {
-  	if ($(this).hasClass('live')) {
-  		var cellData = $(this).text();
-	  	//console.log(cellData);
-	  	//console.log('clicked!');
-	  	
-
-  	}
-  	
-  });
+	$('td').on('click', function() {
+		if ($(this).hasClass('live')) {
+			var cellData = $(this).text();
+		  	//console.log(cellData);
+		  	//console.log('clicked!');
+		  	$(this).replaceWith(createInput(cellData));
+		}	
+	});
 
 });
+
+function createInput(placeholder) {
+	return "<input type='text' class='form-control' type='text' placeholder='" + placeholder + "'>";
+}
