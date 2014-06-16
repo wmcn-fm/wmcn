4cb44e5eefeea2a09d7725607b24b212bde25b34
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-06-16 14:33:18 -0700

    reading td contents onLive

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
-  //identify each element in the table by its owner, category/type...
+  var live;
+
   $('#edit').on('click', function() {
-  	console.log('shit is popping!');
+  	//console.log('shit is popping!');
   	$('.editable').toggleClass('live');
+  	live = $('.live');
+
+  });
+
+
+  $('td').on('click', function() {
+  	if ($(this).hasClass('live')) {
+  		var cellData = $(this).text();
+	  	//console.log(cellData);
+	  	//console.log('clicked!');
+	  	
+
+  	}
+  	
   });
 
 });
