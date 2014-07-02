58b1377d8c8f6f4ef6350ed3cb50dfa30fd4b799
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-07-02 13:34:53 -0500

    clean up all js + filestructure

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,65 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongoskin');
 
-/**
-*	GET for basic urls
-**/
+/** 
+*   ====================================================================
+*   '/'
+*/
 
-/* GET home page. */
+//  GET
 router.get('/', function(req, res) {
   res.render('index', { title: 'WMCN: Macalester College Radio' });
 });
 
-/* GET archive page. */
+/** 
+*   ====================================================================
+*   '/archive'
+*/
+
+//  GET
 router.get('/archive', function(req, res) {
 	res.render('archive', {title: "Show Archive" })
 });
 
-// /* GET admin manager. */
-// router.get('/admin', function(req, res) {
-// 	res.render('adminLanding', {title: "Manage useres" })
-// });
+//  POST
+// just js buttons?
 
+/** 
+*   ====================================================================
+*   '/admin'
+*/
+
+// GET
 router.get('/admin/*', function(req, res, next) {
 	res.set('private content');
 	next();
 })
 
+
+
+/** 
+*   '/admin/applicants'
+*/
+
+//  GET
+router.get('/admin/applicants/dj', function(req, res, next) {
+    res.render('admin/applicants/dj-applicants', {title: "Dj Applications" })
+});
+
+router.get('/admin/applicants/staff', function(req, res, next) {
+    res.render('admin/applicants/staff-applicants', {title: "staff Applications" })
+});
+
+
+
+/**
+*   '/admin/users'
+*/
+
+//  GET
 router.get('/admin/users', function(req, res) {
     var db = req.db;
     var collection = req.collection;
-    // collection.find({},{},function(e,docs){
-    //     res.render('adminLanding', {
-    //         "userlist" : docs
-    //     });
-    // });
 
     collection.find().toArray(function (err, items) {
         res.render('admin/users/manageUsers', {
@@ -44,7 +71,13 @@ router.get('/admin/users', function(req, res) {
     });
 });
 
-/* GET edit user */
+
+
+/**
+*   '/admin/users/:id'
+*/
+
+//  GET
 router.get('/admin/users/:id', function(req, res) {
     var id = req.params.id;
     var db = req.db;
@@ -63,31 +96,7 @@ router.get('/admin/users/:id', function(req, res) {
     });
 });
 
-
-/* GET dj app page. */
-router.get('/dj-application', function(req, res) {
-  res.render('applications/dj', { title: 'Apply to Be a DJ!'});
-});
-
-/* GET staff app page. */
-router.get('/staff-application', function(req, res) {
-  res.render('applications/staff', { title: 'Apply for a WMCN Staff Position' })
-});
-
-/* GET playlist creator. */
-router.get('/post/playlist', function(req, res) {
-	res.render('createPlaylist', {title: "Create a Playlist" })
-});
-
-/* GET post creator. */
-router.get('/post/other', function(req, res) {
-	res.render('createPost', {title: "Create a post" })
-});
-
-/**
-*	POST
-**/
-
+//  POST
 router.post('/updateUser', function(req, res) {
     var db = req.db;
     var collection = req.collection;
@@ -134,7 +143,63 @@ router.post('/updateUser', function(req, res) {
         }); 
 });
 
-router.post('/dj-application', function(req, res) {
+//  DELETE
+router.delete('/admin/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = req.collection;
+    var userToDelete = req.params.id;
+    collection.removeById(userToDelete, function(err, result) {
+        //res.send((result === 1) ? {msg : ''} : {msg:'error: ' + err});
+        if (err) {
+            res.send('error: ' + err)
+        } else {
+            res.send('')
+            //res.location('admin/users');
+            //res.redirect('admin/users');
+        }
+    });
+});
+
+
+
+
+/**
+*   '/admin/scheduler'
+*/
+
+//  GET
+router.get('/admin/scheduler', function(req, res) {
+    res.render('admin/scheduler', {title: 'schedule creator'})
+});
+
+
+
+/**
+*   '/admin/site'
+*/
+
+//  GET
+router.get('/admin/site', function(req, res) {
+    res.render('admin/site', {title: 'site config'})
+});
+
+/** 
+*   ====================================================================
+*   '/applications'
+*/
+
+
+//  GET
+router.get('/applications/dj', function(req, res) {
+  res.render('applications/dj', { title: 'Apply to Be a DJ!'});
+});
+
+router.get('/applications/staff', function(req, res) {
+  res.render('applications/staff', { title: 'Apply for a WMCN Staff Position' })
+});
+
+//  POST
+router.post('/applications/dj', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
@@ -142,24 +207,24 @@ router.post('/dj-application', function(req, res) {
     // Get our form values. These rely on the "name" attributes
 
     var djStatus = req.body.djStatus;
-	//var user = _id;
-	var firstName = req.body.firstName;
-	var lastName = req.body.lastName;
-	var email = req.body.email;
-	var phone = req.body.phone;
-	var studentStatus = req.body.studentStatus;
-	var macIdNum = req.body.macIdNum;
-	var iclass = req.body.iclass;
-	var gradYear = req.body.gradYear;
-	var show = req.body.show;
-	var blurb = req.body.blurb;
+    //var user = _id;
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    var studentStatus = req.body.studentStatus;
+    var macIdNum = req.body.macIdNum;
+    var iclass = req.body.iclass;
+    var gradYear = req.body.gradYear;
+    var show = req.body.show;
+    var blurb = req.body.blurb;
 
     // Set our collection
     var collection = req.collection;
 
     // Submit to the DB
     collection.insert({
-    	"djStatus": djStatus,
+        "djStatus": djStatus,
         "access": 0,
         "firstName" : firstName,
         "lastName" : lastName,
@@ -179,35 +244,65 @@ router.post('/dj-application', function(req, res) {
         }
         else {
             // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("admin/users");
+            res.location("../admin/users");
             // And forward to success page
-            res.redirect("admin/users");
+            res.redirect("../admin/users");
         }
     });
 });
 
-router.delete('/admin/deleteuser/:id', function(req, res) {
-    var db = req.db;
-    var collection = req.collection;
-    var userToDelete = req.params.id;
-    collection.removeById(userToDelete, function(err, result) {
-        res.send((result === 1) ? {msg : ''} : {msg:'error: ' + err});
-    });
+/** 
+*   ====================================================================
+*   '/dj'
+*/
+
+//  GET
+router.get('/dj', function(req, res) {
+    res.render('dj/main', {title: "dj home" })
 });
 
 
 
-/**
-*	admin login credentialing
-**/
+/*
+*   '/dj/login'
+*/
+
+//  GET
+router.get('/dj/login', function(req, res) {
+    res.render('dj/login', {title: "dj login" })
+});
+
+
+/*
+*   '/dj/user'
+*/
+
+//  GET
+router.get('/dj/user', function(req, res) {
+    res.render('dj/user', {title: "edit user" })
+});
 
 
 
-router.get('/admin/scheduler', function(req, res) {
-    var db = req.db;
-    db.collection('usercollection').find().toArray(function (err, items) {
-        res.json(items);
-    });
+/*
+*   '/dj/post'
+*/
+
+//  GET
+router.get('/dj/post', function(req, res) {
+    res.render('dj/post', {title: "make a post" })
+});
+
+
+/** 
+*   ====================================================================
+*   '/show'
+*/
+
+//  GET
+router.get('/show/:id', function(req, res) {
+    var id = req.params.id;
+    res.render('show/viewShow', {title: "dj home: " + id })
 });
 
 
