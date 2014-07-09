182178e0ef932688a0be65cdfa96968d45fc5bf0
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-07-09 16:26:58 -0500

    setup approve functionality

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -11,4 +11,25 @@ $(document).ready(function() {
 		$('.updateButton').css('display', 'inline');
 	});
 
+	$('.appApproval').each(function (i) {
+		var toggle = "<input class='approveApp' type='checkbox'>"
+		$(this).text('').append(toggle);
+
+	});
+
+	
+
+	$('.approveApp').on('click', function() {
+		if ($(this).is(':checked')) {
+			var userId = $(this).parent().siblings('.userId').text();
+			approveApplicant(userId);
+		};
+	})
+
 });	//end ready
+
+
+function approveApplicant(id) {
+	console.log('checked!' + id);
+	
+}
