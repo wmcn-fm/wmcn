14fe3966320a6a2dbedd29422f0d2e0521023b38
MegaJ <ryasurethatschocolateicecream@gmail.com>
2014-07-24 16:17:13 -0500

    kind of tired right now, but I know how to do the rest, have no fear!

--- config_passport.js
+++ config_passport.js
@@ -60,8 +60,8 @@ module.exports = function(passport) {
       // User.findOne wont fire unless data is sent back
       process.nextTick( function() {
       	// find a user whose email is the same as the forms email
-		// we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
+				// we are checking to see if the user trying to login already exists
+        userColl.findOne({ 'email' :  email }, function(err, user) {
             // if there are any errors, return the error
             if (err) {
             	return done(err);
@@ -72,20 +72,23 @@ module.exports = function(passport) {
                 return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
             } else {
 
-				// if there is no user with that email
-                // create the user
-                var newUser            = new User();
 
-                // set the user's local credentials
-                newUser.local.email    = email;
-                newUser.local.password = newUser.generateHash(password);
+            	// ALL THIS NEEDS TO BE INSIDE A BCRYPT CALLBACK
 
-				// save the user
-                newUser.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, newUser);
-                }); // end newUser.save
+				// // if there is no user with that email
+    //             // create the user
+    //             var newUser            = new User();
+
+    //             // set the user's local credentials
+    //             newUser.local.email    = email;
+    //             newUser.local.password = newUser.generateHash(password);
+
+				// // save the user
+    //             newUser.save(function(err) {
+    //                 if (err)
+    //                     throw err;
+    //                 return done(null, newUser);
+    //             }); // end newUser.save
             } // end if/else
         }); 
       }); // end process.nextTick
@@ -104,11 +107,11 @@ module.exports = function(passport) {
       passwordField : 'password',
       passReqToCallback : true // allows us to pass back the entire request to the callback
   },
-  function(req, email, password, done) { // callback with email and password from our form
+  function (req, email, password, done) { // callback with email and password from our form
 
 	// find a user whose email is the same as the forms email
 	// we are checking to see if the user trying to login already exists
-      User.findOne({ 'local.email' :  email }, function(err, user) {
+      userColl.findOne({ 'email' :  email }, function (err, user) {
           // if there are any errors, return the error before anything else
           if (err)
               return done(err);
@@ -117,12 +120,13 @@ module.exports = function(passport) {
           if (!user)
               return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 
-		// if the user is found but the password is wrong
-          if (!user.validPassword(password))
-              return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+          // ALL THIS NEEDS TO BE INSIDE A BCRYPT CALLBACK
+					// // if the user is found but the password is wrong
+     //      if (!user.validPassword(password))
+     //          return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-          // all is well, return successful user
-          return done(null, user);
+     //      // all is well, return successful user
+     //      return done(null, user);
       });
 
   }));
