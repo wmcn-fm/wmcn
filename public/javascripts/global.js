a464e7ac9e7335899ee50ae402bfb7c9870bbf23
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-08-20 02:03:56 -0500

    index layout..

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
+
+    $('.playlist').each(function (i) {
+        var colors = ['#bdbdbd', '#ff8900', '#086fa1', '#a31010'];
+        var color = colors[i];
+        $(this).css('background-color', color);
+    });
+
+    $('#schedule').popover();
+    $('.popover-dismiss').popover({
+      trigger: 'focus'
+    });
     $('#updateUserSubmit').on('click', 'a.deleteUser', deleteUser);
 
     if ($('table'.length)) {
         $('table').floatThead();
     }
+
 });
 
 function deleteUser(event) {
