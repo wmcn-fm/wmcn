1b15a710d6a11606dc3299600e3c1f924d804764
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-09-14 22:27:10 -0500

    account creation working, just need to link show to user doc and flush out app!

--- routes/admin.js
+++ routes/admin.js
@@ -62,99 +62,98 @@ router.get('/applicants/dj', function(req, res) {
 //  POST
 router.post('/applicants/dj', function(req, res) {
     var approved = req.body.data;
-    //  loop over every application
+    console.log(approved);
+
+    //  loop over each application
     forEachAsync(approved, function (next, application, index, array) {
         appColl.findById(application, function (err, app) {
             if (err) {res.send('error');} else {
-                // console.log(app);
+                console.log(app.user.email + ': email');
 
-                //  loop over each user in the application
                 forEachAsync(app.user.email, function (next1, usr, ix, arr) {
                     if (usr != '') {
-                        var isNewUser = true;
+                        console.log(usr, ix);   
+                        console.log(app.user.firstName[ix]);
 
-                        //  check to see if the user already exists
-                        userColl.find({email: usr}).toArray(function (err, result) {
-                            if (err) {res.send('error');} else {
 
-                                if (result.length != 0) {   //  user exsts
-                                    isNewUser = false;
+                        userColl.find({email: usr}).toArray( function (err, result) {
+                            if (result.length != 0) {
+                                console.log(usr + ' user exists!! ::::');
+                                console.log(result[0]);
 
-                                    showColl.update({
-                                        "showTitle" : app.show.showTitle,
-                                        "blurb" : app.show.blurb,
-                                        "timeslot" : 9999,
-                                    }, {$push: {hostId: result[0]._id} },
-                                    {upsert: true}, function (err, shw) {
-                                        console.log('added old dJ to show. here is the show: ');
-                                        console.log(shw);
-                                        next1();
-                                    }); //  end showColl.update
+                                showColl.update({
+                                    "showTitle" : app.show.showTitle,
+                                    "blurb" : app.show.blurb,
+                                    "timeslot" : 9999
+                                }, {$push: {hostId: result[0]._id} }, {upsert: true}, function (err, shw) {
+                                    console.log('added old Dj: ' + result[0].email + ' to show ');
+                                });
+                            } else {
+                                var pass = randomString(10, alphanumeric);
+                                console.log(pass);
+                                var mailOptions = {
+                                    from: 'WMCN noreply <wmcn@macalester.edu>', // sender address
+                                    to: usr, // list of receivers
+                                    subject: 'WMCN Login info', // Subject line
+                                    // text: 'Hello world ✔', // plaintext body
+                                    html: '<b>This is a WMCN test email</b>' +
+                                          '<p> Your login email is: ' + usr + '</p>' +
+                                          '<p> This is your temporary password: ' + pass + '</p>' +
+                                          '<p> your name is: ' + app.user.firstName[ix] +'</p>' + 
+                                          '<p> your show is: ' + app.show.showTitle + '</p>'
+                                }
 
-                                } else {    //  user doesnt exist
-                                    console.log('user doesnt exist!');
-                                    var pass = randomString(10, alphanumeric);
-                                    var mailOptions = {
-                                        from: 'WMCN noreply <noreply@wmcn.fm>', // sender address
-                                        to: usr, // list of receivers
-                                        subject: 'WMCN Login info', // Subject line
-                                        html: '<b>This is a WMCN test email</b>' +
-                                              '<p> Your login email is: ' + usr + '</p>' +
-                                              '<p> This is your temporary password: ' + pass + '</p>' +
-                                              '<p> your name is: ' + app.user.firstName[ix] +'</p>' + 
-                                              '<p> your show is: ' + app.show.showTitle + '</p>'
+                                transporter.sendMail(mailOptions, function (error, info){
+                                    if(error){
+                                        console.log(error);
+                                    }else{
+                                        console.log('Message sent: ' + info.response);
                                     }
-                                    transporter.sendMail(mailOptions, function (error, info){
-                                        if(error){
-                                            console.log(error);
-                                        }else{
-                                            console.log('Message sent: ' + info.response);
-                                        }
-                                    });
+                                });
+                                
+                                bcrypt.hash(pass, null, null, function (err, hash) {
+                                    userColl.insert({
+                                        "access" : 1,
+                                        "firstName" : app.user.firstName[ix],
+                                        "lastName" : app.user.lastName[ix],
+                                        "email" : usr,
+                                        "phone" : app.user.phone[ix],
+                                        "macIdNum" : app.user.macIdNum[ix],
+                                        "iclass" : app.user.iclass[ix],
+                                        "gradYear" : app.user.gradYear[ix],
+                                        "hash" : hash
+                                    }, function (err, newUser) {
+                                        if (err) {res.send('error');} else {
+                                            console.log('new user id: ' + newUser[0]._id);
+                                            showColl.update({
+                                                "showTitle" : app.show.showTitle,
+                                                "blurb" : app.show.blurb,
+                                                "timeslot" : 9999
+                                            }, {$push: {hostId: newUser[0]._id} },
+                                            {upsert: true}, function (err, shw) {
+                                                console.log(shw);
+                                            }); //  end showColl.update
+                                        }   //  end if/else error
+                                    }); //  end userColl.insert cb
+                                }); //  end bcrypt.hash   
+                            }
+                        });
 
-                                    bcrypt.hash(pass, null, null, function (err, hash) {
-                                        userColl.insert({
-                                            "access" : 1,
-                                            "firstName" : app.user.firstName[ix],
-                                            "lastName" : app.user.lastName[ix],
-                                            "email" : usr,
-                                            "phone" : app.user.phone[ix],
-                                            "macIdNum" : app.user.macIdNum[ix],
-                                            "iclass" : app.user.iclass[ix],
-                                            "gradYear" : app.user.gradYear[ix],
-                                            "hash" : hash
-                                        }, function (err, newUser) {
-                                            if (err) {res.send('error');} else {
-                                                console.log('created new user, id: ' + newUser[0]._id);
-                                                showColl.update({
-                                                    "showTitle" : app.show.showTitle,
-                                                    "blurb" : app.show.blurb,
-                                                    "timeslot" : 9999
-                                                }, {$push: {hostId: newUser[0]._id} },
-                                                {upsert: true}, function (err, shw) {
-                                                    console.log('added *new* user to show. here is the show: ');
-                                                    console.log(shw);
-                                                    next1();
-                                                }); //  end showColl.update
-                                            }   //  end if/else error
-                                        }); //  end userColl.insert cb
-                                    }); //  end bcrypt.hash
-                                }   //  end user exists else
-                            }   //  end err if/else
-                        }); //  end userColl.find
-                    }   //  end if usr non-null
-                }).then( function() {
-                    console.log('async2 done'); 
-                    next(); //  next application
+                        
+                    }
+                    next1();
+                }).then( function () {
+                    next();
+                    console.log('async 2 done!');
                 });
-            }   //  end err if/else
+            } //    end appColl error if/else
         }); //  end appColl.find
-    }).then( function() {
+
+    }).then( function () {
         console.log('all done!');
         res.send('http://localhost:3000/admin/users');
-    });
-
-}); //  end router.post
+    }); //  end forEachAsync(approved)
+});
 
 
 
