c4aa7e219dbfcef78cf328dda5380d2c93311b4d
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-04-22 17:01:09 -0500

    update gulpfile, reorganize frontend dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+var paths = {
+  dev: {
+    scripts: 'public/javascripts/*.js',
+    scss: 'public/scss/*.scss',
+    images: 'public/images'
+  },
+  build: {
+    js: 'public/build/js',
+    css: 'public/build/css',
+    images: 'public/build/images'
+  }
+  
+}
 
 var handleError = function(err) { console.log(err); if (this.emit) { this.emit('end'); }};
 
-gulp.task('sass', function () {
-  gulp.src('./public/scss/*.scss')
-    .pipe(sass({onError: handleError}))
-    .pipe(gulp.dest('./public/stylesheets'));
+gulp.task('clean', function(cb) {
+  del([paths.build.js, paths.build.css, paths.build.images], cb);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('sass', ['clean'], function () {
+  gulp.src(paths.dev.scss)
+    .pipe(sass({onError: handleError}))
+    .pipe(minifyCss())
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest(paths.build.css));
+});
+
+gulp.task('watch', function() {
+  // gulp.watch(paths.scripts, ['scripts']);
+  // gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.dev.scss, ['sass']);
+});
+
+
+
+gulp.task('default', ['watch', 'sass'], function() {
+  console.log('\nbuild succeeded!\n');
+});
