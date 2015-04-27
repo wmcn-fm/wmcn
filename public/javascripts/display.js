e82619d395b957828453b0ba669cc5acbc9f6fa4
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-04-27 02:15:52 -0500

    add player, clear nav

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
 
+  alignNav();
+  $(window).resize(function() {
+    alignNav();
+  });
+
   //  convert ISO 8601 to "from now"
   $('.dateFromNow').each(function(i) {
     var hours = moment($(this).text()).fromNow();
     $(this).text(hours);
   }); 
 });
+
+
+function alignNav() {
+  var height = $( window ).height() - ($('#main-nav').height() + 7);
+  $('#header, #clear').css({"height": height});
+}
